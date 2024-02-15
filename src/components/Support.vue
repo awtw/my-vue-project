dc8115285f01e55190bbe0b3b72b0bba05fba718
August Wang <august.wang.tpe@gmail.com>
2024-02-16 00:32:06 +0800

    add support page init

--- src/components/Support.vue
+++ src/components/Support.vue
@@ -5,9 +5,12 @@
 <template>
     <section class="pt-20 bg-white dark:bg-gray-900 pb-96 h-100">
         <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-            <div class="mx-auto max-w-screen-sm text-center">
-                <h1 class="mb-4 text-2xl font-extrabold text-primary-600 dark:text-primary-500">Sorry about that ...</h1>
-                <p class="mb-10 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Whoops! The page is not completed yet</p>
+            <div class="mx-auto max-w-screen-sm text-left">
+                <h1 class="mb-4 text-xl lg:text-2xl font-extrabold text-primary-600 dark:text-primary-500" >網頁維護中...</h1>
+                <p class="mb-3 text-xl lg:text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">您可以透過以下方式支持我持續寫專案</p>
+                <p class="mb-10 text-xl lg:text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">你們每一分小小的金額對我都是大大的支持</p>
+                <img class="mx-0 mb-43 w-atuo h-72 rounded-lg" src="https://vueproject.s3.us-west-2.amazonaws.com/S__37650520.jpg" alt="wevhat wallet">
+
             </div>   
         </div>
     </section>
